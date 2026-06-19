7d9ec89e8d98b2f8fe256133488c4e430e9cb136
Saswat024 <saswats024@gmail.com>
2026-06-19 21:15:02 +0530

    Second commit

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,4 @@
-import Chat from "../models/chat.js";
+import Chat from "../models/Chat.js";
 import User from "../models/User.js";
 import imagekit from "../configs/imageKit.js";
 import openai from "../configs/openai.js";
--- server/server.js
+++ server/server.js
@@ -1,33 +1,37 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import connectDB from './configs/db.js'
-import userRouter from './routes/userRoutes.js'
-import chatRouter from './routes/chatRoutes.js'
-import messageRouter from './routes/messageRoutes.js'
-import creditRouter from './routes/creditsRoutes.js'
-import { stripeWebhooks } from './controllers/webhooks.js'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import chatRouter from "./routes/chatRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import creditRouter from "./routes/creditsRoutes.js";
+import { stripeWebhooks } from "./controllers/webhooks.js";
 
-const app = express()
+const app = express();
 
-await connectDB()
+await connectDB();
 
 // Stripe webhooks
-app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks )
+app.post(
+  "/api/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhooks,
+);
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send('Server is Live!'))
-app.use('/api/user', userRouter)
-app.use('/api/chat', chatRouter)
-app.use('/api/message', messageRouter)
-app.use('/api/credit', creditRouter)
- 
-const PORT = process.env.PORT || 3000
+app.get("/", (req, res) => res.send("Server is Live!"));
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
+app.use("/api/credit", creditRouter);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
